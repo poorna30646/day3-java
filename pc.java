acd5f296b6bca692ce90350f34a49f53afd8d370
poorna30646 <thotipoornachandra@gmail.com>
2024-12-04 11:10:00 +0530

    Add files via upload

---
+++ pc.java
@@ -0,0 +1,15 @@
+class pub
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int pos=2;
+        if((a&pos)==1)
+        {
+            System.out.println("SET BIT");
+        }
+        else{
+            System.out.println("NOT SET BIT");
+        }
+    }
+}
