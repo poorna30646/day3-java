56b577b6b97fd9c7c88601e44750d0fb8c4ffed8
poorna30646 <thotipoornachandra@gmail.com>
2024-12-04 11:02:28 +0530

    Add files via upload

---
+++ poorna.java
@@ -0,0 +1,15 @@
+class bitwise
+{
+    public static void main(String args[])
+    {
+        int a=17;
+        if((a&1)==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}
