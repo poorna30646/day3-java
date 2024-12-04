35001ce8166754ee8970187966c1ee5ad862634d
poorna30646 <thotipoornachandra@gmail.com>
2024-12-04 12:27:08 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,18 @@
+class bitwise
+{
+    public static void main(String args[])
+    {
+        int a=20;
+        int b=110;
+        if(a<b)
+        {
+            System.out.println("a is big");
+        }
+        else
+        {
+            System.out.println("b is big");
+
+        }
+    }
+
+}
